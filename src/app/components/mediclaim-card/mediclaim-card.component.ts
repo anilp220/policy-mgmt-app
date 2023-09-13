8270a2089eba9652cc7e2f73870089bf09fbb93b
anil patel <anilp220@gmail.com>
2023-09-13 18:30:45 +0530

    update

--- src/app/components/mediclaim-card/mediclaim-card.component.ts
+++ src/app/components/mediclaim-card/mediclaim-card.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MediclaimCardComponent implements OnInit {
   @Input() data = [];
   @Input() investorName;
+  @Input() pageTitle;
   constructor() { }
 
   ngOnInit() {
